9b4913868324d39fdb34685c15699d02baad1582
Andrés Pereira <apereira@alabs.org>
2015-11-23 11:49:37 +0100

    [deploy] solo master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server 'gobiernoabierto.carchi.gob.ec', roles: %w{web app db}
-set :branch, "staging"
+set :branch, "master"
 set :deploy_to, "/var/www/gobiernoabierto.carchi.gob.ec"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 server 'beta.gobiernoabierto.carchi.gob.ec', roles: %w{web app db}
-set :branch, "staging"
+set :branch, "master"
 set :deploy_to, "/var/www/beta.gobiernoabierto.carchi.gob.ec"
